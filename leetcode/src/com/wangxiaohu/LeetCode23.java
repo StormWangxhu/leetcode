f179bac424b0506d4c4ced835073e13017a7f38c
StormWangxhu <StormWangxhu@163.com>
2021-12-11 18:48:06 +0800

    add:leetcode23[合并K个有序链表][最小堆][优先级队列]
    技巧：
    1、最小堆
    2、构建虚拟头节点

---
+++ leetcode/src/com/wangxiaohu/LeetCode23.java
@@ -0,0 +1,61 @@
+package com.wangxiaohu;
+
+import java.util.PriorityQueue;
+
+public class LeetCode23 {
+
+    /**
+     * 合并 K 个升序链表
+     * 参考：https://labuladong.gitee.io/algo/2/17/16/
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        // // 优先级队列，最小堆
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                lists.length, (a, b) -> (a.val - b.val));
+
+        // 将 k 个链表的头结点加入最小堆
+        for (ListNode head : lists) {
+            if (head != null)
+                pq.add(head);
+        }
+
+        while (!pq.isEmpty()) {
+            // 获取最小节点，接到结果链表中
+            ListNode node = pq.poll();
+            p.next = node;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+        return dummy.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
